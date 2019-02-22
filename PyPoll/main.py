4a3e7082adae05201b1446ab1c89af9eb5957bff
jchung243 <jchung243@gmail.com>
2019-02-22 18:54:08 -0500

    PyPoll: fully functional with correct data TODO: add write to text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,9 +10,10 @@ with open(csv_path, newline='') as election_csv:
     # Separate header and call next line
     election_header = next(election_data)
 
-    # Define empty dictionary
+    # Define empty dictionary and placeholder variables
     votes = {}
     votesum = 0
+    votecount = 0
 
     # Loop through rows of data
     for row in election_data:
@@ -23,13 +24,33 @@ with open(csv_path, newline='') as election_csv:
         else:
             votes[candidate] = 1
             
-    # Loop through dictonary
+    # Loop through dictonary to tally sum of votes and find winner
     for key in votes:
         votesum = votesum + int(votes[key])
+        if votes[key] > votecount:
+            winner = key
+            votecount = votes[key]
 
-    # Print statments
-    print(votes)
-    print(votesum)
+    # Print header and total votes
+    print("Election Results")
+    print("-------------------------")
+
+    # Loop though dictonary to print election results
+    for key in votes:
+        print(f"{key}: {round(100*(votes[key]/votesum),3)}% ({votes[key]})")
+    
+    # Print winner
+    print("-------------------------")
+    print(f"Winner: {winner}")
+    print("-------------------------")
+
+# Khan: 63.000% (2218231)
+# Correy: 20.000% (704200)
+# Li: 14.000% (492940)
+# O'Tooley: 3.000% (105630)
+# -------------------------
+# Winner: Khan
+# -------------------------
 
     # Export to text file
     #output = open("PyBank_Output.txt", "w")
