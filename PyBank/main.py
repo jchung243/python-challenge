6834c843c2c32a4ab1cf759399abff374a823e74
jchung243 <jchung243@gmail.com>
2019-02-22 18:05:43 -0500

    PyBank: added output to text file, challenge complete.

--- PyBank/main.py
+++ PyBank/main.py
@@ -47,32 +47,17 @@ with open(csv_path, newline='') as budget_csv:
     print("----------------------------")
     print(f"Total Months: {len(profit_tracker)}")
     print(f"Total: ${sum(profit_tracker)}")
-    print(f"Average Change: ${sum(profit_change)/len(profit_change)}")
+    print(f"Average Change: ${round(sum(profit_change)/len(profit_change), 2)}")
     print(f"Greatest Increase in Profits: {increase_date} (${increase})")
     print(f"Greatest Decrease in Profits: {decrease_date} (${decrease})")
 
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
-
-  # The total number of months included in the dataset
-
-  # The net total amount of "Profit/Losses" over the entire period
-
-  # The average of the changes in "Profit/Losses" over the entire period
-
-  # The greatest increase in profits (date and amount) over the entire period
-
-  # The greatest decrease in losses (date and amount) over the entire period
-
-# As an example, your analysis should look similar to the one below:
-
-  #```text
-  #Financial Analysis
-  #----------------------------
-  #Total Months: 86
-  #Total: $38382578
-  #Average  Change: $-2315.12
-  #Greatest Increase in Profits: Feb-2012 ($1926159)
-  #Greatest Decrease in Profits: Sep-2013 ($-2196167)
-  #```
-
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+    # Export to text file
+    output = open("PyBank_Output.txt", "w")
+    output.write("Financial Analysis\n")
+    output.write("----------------------------\n")
+    output.write(f"Total Months: {len(profit_tracker)}\n")
+    output.write(f"Total: ${sum(profit_tracker)}\n")
+    output.write(f"Average Change: ${round(sum(profit_change)/len(profit_change), 2)}\n")
+    output.write(f"Greatest Increase in Profits: {increase_date} (${increase})\n")
+    output.write(f"Greatest Decrease in Profits: {decrease_date} (${decrease})\n")
+    output.close()
