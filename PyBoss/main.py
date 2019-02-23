5a9de1eea36b50ca9aea2b2897f7e7ef32f05d49
jchung243 <jchung243@gmail.com>
2019-02-23 12:45:12 -0500

    PyBoss: added csv writing, challenge completed

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -56,31 +56,43 @@ us_state = {
     'Wyoming': 'WY',
 }
 
-# Open CSV
+# CSV Paths
 csv_path = os.path.join('..', 'Resources', 'employee_data.csv')
+write_path = os.path.join(".", "employee_data_OUTPUT.csv")
+
 with open(csv_path, newline='') as employee_csv:
     employee_data = csv.reader(employee_csv, delimiter=',')
 
     # Separate header and call next line
     employee_header = next(employee_data)
 
-    # For loop to cycle through data
-    for row in employee_data:
+    # Open the file using "write" mode. Specify the variable to hold the contents
+    with open(write_path, 'w', newline='') as csvfile:
+
+        # Initialize csv.writer
+        output = csv.writer(csvfile, delimiter=',')
+
+        # Write headers
+        output.writerow(['Employee ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State'])
+    
+        # For loop to cycle through data
+        for row in employee_data:
         
-        # Separate first and last name
-        first,last = row[1].split(" ")
-        row[1] = first
-        row.insert(2, last)
+            # Separate first and last name
+            first,last = row[1].split(" ")
+            row[1] = first
+            row.insert(2, last)
 
-        # Change DOB
-        year,month,day = row[3].split("-")
-        row[3] = f"{month}/{day}/{year}"
+            # Change DOB
+            year,month,day = row[3].split("-")
+            row[3] = f"{month}/{day}/{year}"
 
-        # Change SSN
-        first,second,third = row[4].split("-")
-        row[4] = f"***-***-{third}"
+            # Change SSN
+            first,second,third = row[4].split("-")
+            row[4] = f"***-***-{third}"
 
-        # Lookup State and change
-        row[5] = us_state[row[5]]
+            # Lookup State and change
+            row[5] = us_state[row[5]]
 
-    print(row)
+            # Write row to CSV
+            output.writerow(row)
