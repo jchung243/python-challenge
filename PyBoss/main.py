c72d663b6b3090392d9b1f0bcc8a563c8f97a00b
jchung243 <jchung243@gmail.com>
2019-02-23 12:30:00 -0500

    PyBoss: first and last names now separate

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -2,6 +2,60 @@
 import os
 import csv
 
+# State Dictionary
+us_state = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
 # Open CSV
 csv_path = os.path.join('..', 'Resources', 'employee_data.csv')
 with open(csv_path, newline='') as employee_csv:
@@ -10,6 +64,11 @@ with open(csv_path, newline='') as employee_csv:
     # Separate header and call next line
     employee_header = next(employee_data)
 
-    #For loop to cycle through data
+    # For loop to cycle through data
     for row in employee_data:
-        print('hi')
+        
+        # Separate first and last name
+        first,last = row[1].split(" ")
+        row[1] = first
+        row.insert(2, last)
+    print(row)
